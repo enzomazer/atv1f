12356da53115248c52d143fd3277f8cc9dce607f
enzomazer <enzokmitani@gmail.com>
2025-09-16 13:55:28 -0400

    d
    d

--- script.js
+++
@@ -1,6 +0,0 @@
-function funcao() {
-let um = parseFloat(document.getElementById("um").value)
-let dois = parseFloat(document.getElementById("dois").value)
-let soma = um + dois
-document.getElementById("resposta").innerHTML = soma
-}
